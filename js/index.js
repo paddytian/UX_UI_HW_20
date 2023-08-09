adcbbcf3cae966995666a34ffdf24923f7fe06af
paddytian <patrick_tian1108@hotmail.com>
2023-08-09 17:00:14 -0400

    added

---
+++ js/index.js
@@ -0,0 +1,41 @@
+const el = document.getElementById('back1');
+
+const hiddenDiv = document.getElementById('smallsdhs');
+
+el.addEventListener('mouseover', function handleMouseOver() {
+  hiddenDiv.style.display = 'block';
+
+});
+
+el.addEventListener('mouseout', function handleMouseOut() {
+  hiddenDiv.style.display = 'none';
+
+});
+
+const el1 = document.getElementById('back2');
+
+const hiddenDiv1 = document.getElementById('smallftc');
+
+el1.addEventListener('mouseover', function handleMouseOver() {
+  hiddenDiv1.style.display = 'block';
+
+});
+
+el1.addEventListener('mouseout', function handleMouseOut() {
+  hiddenDiv1.style.display = 'none';
+
+});
+
+const el2 = document.getElementById('back3');
+
+const hiddenDiv2 = document.getElementById('smallseatme');
+
+el2.addEventListener('mouseover', function handleMouseOver() {
+  hiddenDiv2.style.display = 'block';
+
+});
+
+el2.addEventListener('mouseout', function handleMouseOut() {
+  hiddenDiv2.style.display = 'none';
+
+});
